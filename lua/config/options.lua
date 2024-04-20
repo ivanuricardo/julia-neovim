284bb79864b4e14c350a9629c200245dc51e7ce5
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-04-20 13:14:49 +0200

    Playing with Flash

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+
+opt.signcolumn = "yes" -- Automatically adjust sign column
 opt.scrolloff = 7 -- Lines of context
 opt.wrap = true -- Disable line wrap
 opt.shiftwidth = 4 -- Size of indent
-opt.colorcolumn = "80"
-opt.signcolumn = "no"
+opt.colorcolumn = "80" -- Line length marker
+
+vim.o.list = true
